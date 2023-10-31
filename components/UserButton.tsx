4be9337bc575691de21bccdca2c1194ed0032834
Brandon Trump <brandontrump101@gmail.com>
2023-10-31 16:58:19 +1300

    feat: completed customer portal redirect to manage subscription plan

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -11,12 +11,18 @@ import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
 import { Button } from "./ui/button";
+import { useSubscriptionStore } from "@/store/store";
+import LoadingSpinner from "./LoadingSpinner";
+import { StarIcon } from "lucide-react";
+import ManageAccountButton from "./ManageAccountButton";
 
 interface UserButtonProps {
   session: Session | null;
 }
 
 function UserButton({ session }: UserButtonProps) {
+  const subscription = useSubscriptionStore((state) => state.subscription);
+
   if (!session) {
     return (
       <Button variant={"outline"} onClick={() => signIn("google")}>
@@ -33,6 +39,27 @@ function UserButton({ session }: UserButtonProps) {
         <DropdownMenuContent>
           <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
+
+          {subscription === undefined && (
+            <DropdownMenuItem>
+              <LoadingSpinner />
+            </DropdownMenuItem>
+          )}
+
+          {subscription?.role === "pro" && (
+            <>
+              <DropdownMenuLabel className="text-xs flex items-center justify-center space-x-1 text-[#E935C1] animate-pulse">
+                <StarIcon fill="#E935C1" />
+                <p>PRO</p>
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem>
+                <ManageAccountButton />
+              </DropdownMenuItem>
+            </>
+          )}
 
           <DropdownMenuItem onClick={() => signOut()}>
             Sign Out
