b5b618a8557189bccb20a2bb733cf83adec879a9
Brandon Trump <brandontrump101@gmail.com>
2023-11-01 03:37:15 +1300

    feat: displays permission error if accessing unauthorised chats

--- app/(user)/chat/page.tsx
+++ app/(user)/chat/page.tsx
@@ -1,4 +1,5 @@
 import ChatList from "@/components/ChatList";
+import ChatPermissionError from "@/components/ChatPermissionError";
 
 type ChatsPageProps = {
   params: {};
@@ -8,6 +9,11 @@ type ChatsPageProps = {
 function ChatsPage({ searchParams: { error } }: ChatsPageProps) {
   return (
     <div>
+      {error && (
+        <div className="m-2">
+          <ChatPermissionError />
+        </div>
+      )}
       <ChatList />
     </div>
   );
