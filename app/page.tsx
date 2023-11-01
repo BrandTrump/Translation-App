bca3a2471a33ccba4d6167927189109b4f89d5c3
Brandon Trump <brandontrump101@gmail.com>
2023-11-01 14:29:30 +1300

    style: added styling to image

--- app/page.tsx
+++ app/page.tsx
@@ -50,10 +50,12 @@ export default function Home() {
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
+                  unoptimized
                   src={DemoImg}
                   width={2432}
                   height={1442}
                   alt="app screenshot"
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
                 />
                 {/* <div className="bg-white w-full h-[1200px] rounded-md shadow-2xl ring-1 ring-gray-900/10"></div> */}
               </div>
